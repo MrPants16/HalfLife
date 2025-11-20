3c02891ba4b0a86896ffdf020e6bc82f51199c7b
Rylan Rees <ryguyrees@gmail.com>
2025-11-19 21:27:11 -0700

    1.0

--- main.py
+++ main.py
@@ -1,41 +1,32 @@
 import matplotlib.pyplot as plt
 # formula = quantity_remaining: float = quantity * 0.5 ** (time_elapsed / half_life)
 
-half_life: float = 7 # days
-quantity: float = 120 # millagrams
-in_system: float = 0
-freq: float = 3.5
-intake_length: int = 28 * 2
-intake: list = []
-day_list: list = []
-quantity_list: list = []
+def halfLife(half_life, amount, times_week) -> tuple[list, list]:
+    freq: float = 7 / times_week
+    cycle_length: int = 28
+    day_list: list = list(range(cycle_length))
+    dose_days: list = [int(round(freq * i)) for i in range(int(times_week * (cycle_length / 7)))]
+    life_list: list = [0.0] * cycle_length
 
-# Set up the lights and add quanity to the intake days
-for take in range(intake_length):
-    quantity_list.append(0)
-    day_list.append(take)
-    intake.append(int(freq * take))
-    if take in intake:
-        quantity_list[take] += quantity
-    for i in range(int(half_life) * 7):
-        try:
-            quantity_list[take + i] += quantity * 0.5 ** (take / half_life)
-        except:
-            continue
-            
-cycles_five: float = intake[-1] * 5
-quantity_list[0] = quantity
+    # loop to iterate day then loop of mg on those days
+    for day in range(cycle_length):
+        if day > 0:
+            life_list[day] = life_list[day -1] * (0.5 ** (1 / half_life))
+        if day in dose_days:
+            life_list[day] += amount
+    return (day_list, life_list)
 
-for i in range(max(len(day_list), len(quantity_list))):
-    print(f"{day_list[i]} : {quantity_list[i]:.2f},", end=" ")
-    pass
-
-def plotGraph():
-    plt.plot(0, 0, color="white") # forces graph to go to 0 on the x and y axis
-    plt.plot(day_list, quantity_list, color="blue")
+def plotGraph(day_list, quantity_list) -> None:
+    plt.plot(day_list, quantity_list, color="blue") 
+    plt.ylim(0, max(quantity_list) + max(quantity_list) / 10)
     plt.xlabel("Time (day)")
     plt.ylabel("Substance left (mg)")
     plt.title("Half life grapher")
     plt.show()
-    
-plotGraph()
+
+# half life in days, mg of substance, times per week
+days, doses = halfLife(7, 120, 2)
+print(doses)
+plotGraph(days, doses)
+# days, doses = halfLife(0.5, 6.0, 100)
+# plotGraph(days, doses)
